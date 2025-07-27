636e30b6cbb5985299f08a29c08126cdf8163d18
Cristopherch <cristopher.chamorro@epn.edu.ec>
2025-07-27 10:23:48 -0500

    Delete usuario.h

--- usuario.h
+++
@@ -1,8 +0,0 @@
-#ifndef USUARIO_H
-#define USUARIO_H
-
-void registroUsuario(const char* cedula, const char* contrasena);
-	
-int validacionUsuario(const char* cedula, const char* contrasena);
-
-#endif
